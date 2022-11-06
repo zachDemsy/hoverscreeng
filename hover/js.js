2c04484baaa98fc1e04ada8ed06b42218c52467f
Ahmed1084 <109950387+Ahmed1084@users.noreply.github.com>
2022-11-06 15:28:23 +0530

    Update js.js

--- hover/js.js
+++ hover/js.js
@@ -1,5 +1,7 @@
-function aud_play_pause() {
-    var myAudio = document.getElementsById("fa fa-play");
+
+var myAudio = document.getElementsById("fa fa-play");
+myAudion.onclick = function aud_play_pause() {
+    
     if (myAudio.paused) {
         $('#stateicon').removeClass('fa fa-play');
         $('#stateicon').addClass('fa fa-pause');
