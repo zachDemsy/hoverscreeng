86ec0ee6f56ae5158318d10f6cf26e90c949d2cd
Ahmed1084 <109950387+Ahmed1084@users.noreply.github.com>
2022-11-06 16:51:17 +0530

    Create js.js

---
+++ js.js
@@ -0,0 +1,12 @@
+var myAudio = document.getElementById("play");
+function aud_play_pause() {
+    if (myAudio.value == undefined) myAudio.value = "0";
+    if (myAudio.value === "0") {
+        myAudio.setAttribute("class", 'fa fa-pause');
+        myAudio.value = "1";
+    } else {
+        myAudio.setAttribute("class", 'fa fa-play');
+        myAudio.value = "0";
+    } 
+}
+
